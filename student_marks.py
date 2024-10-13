278ee53e782fe1f43b44fc561d9872798f75e510
Abhisek Dey <abhisek.datascience@gmail.com>
2024-10-13 10:09:08 +0530

    Second Commit

---
+++ student_marks.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 13 10:03:51 2024
+
+@author: chata
+"""
+
+list_student = [
+    
+    {
+     id:1,
+     name:'John',
+     grade:'A'
+     
+     },
+    
+    {
+     id:2,
+     name:'Merry',
+     grade:'A'
+     
+     },
+    
+    {
+     id:2,
+     name:'Nancy',
+     grade:'B'
+     
+     }
+    
+    ]
